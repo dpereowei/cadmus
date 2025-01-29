c7d56142f9f1a248f682681da291f18b2f606f62
Real-Artisan <iwenyadaniel12@gmail.com>
2025-01-29 12:28:14 -0600

    build and push to dockerhub

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,9 +1,20 @@
 BRANCH_NAME=${GITHUB_REF#refs/heads/}
 HASH=$(git rev-parse --short "$GITHUB_SHA")
+#!/bin/bash
+
+BRANCH_NAME=${GITHUB_REF#refs/heads/}
+HASH=$(git rev-parse --short "$GITHUB_SHA")
 TIMESTAMP=$(date --utc +%Y%m%dT%H%M%S)
 TAG_NAME="${HASH}_${BRANCH_NAME}_${TIMESTAMP}"
-echo ${TAG_NAME}
+echo "Generated tag: ${TAG_NAME}"
 
-IMAGE_NAME=ghcr.io/${GITHUB_REPOSITORY_OWNER}/$(basename ${GITHUB_REPOSITORY})
+# Define Docker Hub repository (force lowercase)
+IMAGE_NAME="$(echo "${DOCKERHUB_USERNAME}/$(basename "${GITHUB_REPOSITORY}")" | tr '[:upper:]' '[:lower:]')"
 
-docker buildx build --file webapp/Dockerfile --tag $IMAGE_NAME:latest --tag $IMAGE_NAME:${TAG_NAME} --push webapp
+echo "Using image name: $IMAGE_NAME"
+
+# Login to Docker Hub
+echo "${DOCKERHUB_TOKEN}" | docker login -u "${DOCKERHUB_USERNAME}" --password-stdin
+
+# Build and push the image
+docker buildx build --file webapp/Dockerfile --tag "$IMAGE_NAME:latest" --tag "$IMAGE_NAME:$TAG_NAME" --push webapp
